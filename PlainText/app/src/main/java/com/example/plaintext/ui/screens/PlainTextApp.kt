e9f397311f4193b2191b636428b16d3e2b80742e
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2026-06-10 15:58:26 -0400

    feat: passando a preferencia view para as duas telas

--- PlainText/app/src/main/java/com/example/plaintext/ui/screens/PlainTextApp.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/screens/PlainTextApp.kt
@@ -1,24 +1,16 @@
 package com.example.plaintext.ui.screens
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.toRoute
 import com.example.plaintext.data.model.PasswordInfo
 import com.example.plaintext.ui.screens.editList.EditList
 import com.example.plaintext.ui.screens.hello.Hello_screen
-import com.example.plaintext.ui.screens.list.AddButton
 import com.example.plaintext.ui.screens.list.ListView
 import com.example.plaintext.ui.screens.login.Login_screen
-import com.example.plaintext.ui.screens.login.TopBarComponent
 import com.example.plaintext.ui.screens.preferences.SettingsScreen
-import com.example.plaintext.ui.viewmodel.ListViewModel
 import com.example.plaintext.ui.viewmodel.PreferencesViewModel
 import com.example.plaintext.utils.parcelableType
 import kotlin.reflect.typeOf
@@ -27,6 +19,8 @@ import kotlin.reflect.typeOf
 fun PlainTextApp(
     appState: JetcasterAppState = rememberJetcasterAppState()
 ) {
+    val preferencesViewModel: PreferencesViewModel = hiltViewModel()
+
     NavHost(
         navController = appState.navController,
         startDestination = Screen.Login,
@@ -43,12 +37,14 @@ fun PlainTextApp(
                 },
                 navigateToList = {
                     appState.navController.navigate(Screen.List)
-                }
+                },
+                viewModel = preferencesViewModel
             )
         }
         composable<Screen.Preferences> {
             SettingsScreen(
-                navController = appState.navController
+                navController = appState.navController,
+                viewModel = preferencesViewModel
             )
         }
         composable<Screen.List> {
